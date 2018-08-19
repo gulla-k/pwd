22435c812517bc50d8be0517b80970c37071208b
Gulla-k <gulla.k@yandex.ru>
2018-08-19 10:40:28 +0300

    Изменения в web/urls.py

--- web/urls.py
+++ web/urls.py
@@ -17,6 +17,7 @@ from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
+	path('', include('home.urls')),
 	path('blog/', include('blog.urls')),
     path('panel/', admin.site.urls),
 ]
